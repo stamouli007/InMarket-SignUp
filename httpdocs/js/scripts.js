83a4f9a879dfc1e22f8ab912146a7787240149be
Ian Bohorquez <bohorquez@source-marketing.com>
2012-06-06 16:53:45 -0400

    2012-06-06 16:55:00

--- httpdocs/js/scripts.js
+++ httpdocs/js/scripts.js
@@ -1,7 +1,18 @@
 
 $(document).ready(function() {
 	
+	$("li.refresh").click(function() {
+		location.reload(true);
+	});
 	
+	
+	// form submit
+	$("#btn-submit").click(function() {
+		
+		// submit the form
+		//alert("submit form");
+		
+	});
 	
 	
 	
